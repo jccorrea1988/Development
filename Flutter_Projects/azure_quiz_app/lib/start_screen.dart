603c9832b4900ce64f6573301f886a68d7d232dc
jccorrea1988 <98226737+jccorrea1988@users.noreply.github.com>
2023-12-05 09:37:17 -0300

    Update Quiz

--- Flutter_Projects/azure_quiz_app/lib/start_screen.dart
+++ Flutter_Projects/azure_quiz_app/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +33,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start Quiz'),
